610dab75c02d90a615cb485e42318507bd6e5545
sethhatchel <shhatch@ilstu.edu>
2019-04-28 12:58:50 -0500

    Added activities and toolbar

--- AndroidAlarmProject/app/src/main/java/com/example/alarm/MainActivity.java
+++ AndroidAlarmProject/app/src/main/java/com/example/alarm/MainActivity.java
@@ -13,8 +13,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void gotoLocAlarm(View view){
+    public void gotoLocAlarm(View view) {
         Intent goToLoc = new Intent(this, LocAlarm.class);
         startActivity(goToLoc);
+    }
+
+    public void gotoOneTimeAlarm(View view) {
+        Intent gotoOneTimeAlarm = new Intent(this, OneTimeAlarm.class);
+        startActivity(gotoOneTimeAlarm);
+    }
+
+    public void gotoRecursiveAlarm(View view) {
+        Intent gotoRecursiveAlarm = new Intent(this, RecursiveAlarm.class);
+        startActivity(gotoRecursiveAlarm);
     }
 }
