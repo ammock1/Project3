b36a511f64f9b29c49e1b77c05a1c71f62080c80
Mock, Anne <ammock1.ilstu.edu>
2019-04-30 18:37:34 -0500

    Sends notifications when location alarm received

--- AndroidAlarmProject/app/src/main/java/com/example/alarm/LocAlarmObj.java
+++ AndroidAlarmProject/app/src/main/java/com/example/alarm/LocAlarmObj.java
@@ -1,25 +1,25 @@
 package com.example.alarm;
 
 import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.ServiceConnection;
-import android.os.IBinder;
+import android.os.Build;
 import android.os.PowerManager;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
-
-import com.example.alarm.NotificationService.LocalBinder;
 
 public class LocAlarmObj extends BroadcastReceiver {
     //was going to use singleton design pattern, but android insisted on it having a default constructor
 
     private static LocAlarmObj locAl = new LocAlarmObj();
+    Context mContext;
     private long interval = 5000;
-    private boolean bounded;
-    //NotificationService noteServe = new NotificationService();
+    NotificationChannel channel;
 
     public static LocAlarmObj getInstance(){
         return locAl;
@@ -29,6 +29,7 @@ public class LocAlarmObj extends BroadcastReceiver {
     }
 
     public void setAlarm(Context context){
+        mContext = context;
         AlarmManager am = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
         Intent i = new Intent(context, LocAlarmObj.class);
         PendingIntent pi = PendingIntent.getBroadcast(context, 0, i, 0);
@@ -40,6 +41,7 @@ public class LocAlarmObj extends BroadcastReceiver {
     }
 
     public void cancelAlarm(Context context){
+        mContext = context;
         Intent intent = new Intent(context, LocAlarmObj.class);
         PendingIntent sender = PendingIntent.getBroadcast(context, 0, intent, 0);
         AlarmManager am = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
@@ -47,28 +49,27 @@ public class LocAlarmObj extends BroadcastReceiver {
     }
     @Override
     public void onReceive(Context context, Intent intent){
-        /*Intent mIntent = new Intent(context, NotificationService.class);
-        context.bindService(mIntent, connection, context.BIND_AUTO_CREATE);*/
-        PowerManager pm = (PowerManager)context.getSystemService(Context.POWER_SERVICE);
+       /* PowerManager pm = (PowerManager)context.getSystemService(context.POWER_SERVICE);
         PowerManager.WakeLock w1 = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "wakey:");
-        w1.acquire();
-        //noteServe.showLocNote();
-        Log.i("alarm", "onReceive of LocAlarmObj called");
-        w1.release();
-        //context.unbindService(connection);
-    }
- /*   ServiceConnection connection = new ServiceConnection() {
-        @Override
-        public void onServiceDisconnected(ComponentName name) {
-            bounded = false;
-            noteServe = null;
-        }
+        w1.acquire();*/
+        mContext = context;
 
-        @Override
-        public void onServiceConnected(ComponentName name, IBinder service) {
-            bounded = true;
-            LocalBinder mLocalBinder = (LocalBinder)service;
-            noteServe = mLocalBinder.getInstance();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            channel = new NotificationChannel("Channel1", "noteChannel", NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription("it's a channel");
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
         }
-    };*/
+        NotificationManager notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationCompat.Builder locNoteBuilder = new NotificationCompat.Builder(context, "Channel1")
+                .setContentText("Get up and walk!")
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                ;
+        notificationManager.notify(1, locNoteBuilder.build());
+
+       // w1.release();
+    }
 }
