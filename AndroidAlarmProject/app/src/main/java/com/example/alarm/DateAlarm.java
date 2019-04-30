02dffcf1207963e7932435cf7d040540a9df3cef
sethhatchel <shhatch@ilstu.edu>
2019-04-29 20:36:43 -0500

    Added UI and Logic for Alarm by Date Activity

--- AndroidAlarmProject/app/src/main/java/com/example/alarm/DateAlarm.java
+++ AndroidAlarmProject/app/src/main/java/com/example/alarm/DateAlarm.java
@@ -1,15 +1,23 @@
 package com.example.alarm;
 
+import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
-public class DateAlarm extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
+public class DateAlarm extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener, AdapterView.OnItemSelectedListener, DatePickerDialog.OnDateSetListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,19 +27,77 @@ public class DateAlarm extends AppCompatActivity implements TimePickerDialog.OnT
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        Spinner repeat = findViewById(R.id.repeatSpinner);
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this, R.array.repeatEvery, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        repeat.setAdapter(adapter1);
+        repeat.setOnItemSelectedListener(this);
 
-//        Button button = (Button) findViewById(R.id.newRecursiveButton);
-//        button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                DialogFragment timePicker = new TimePickerFragment();
-//                timePicker.show(getSupportFragmentManager(), "time picker");
-//            }
-//        });
+        Spinner timeZone = findViewById(R.id.timeZoneSpinner);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.timeZone, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        timeZone.setAdapter(adapter2);
+        timeZone.setOnItemSelectedListener(this);
+
+        Button timePickButton = findViewById(R.id.setTimeButton);
+        timePickButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment timePicker = new TimePickerFragment();
+                timePicker.show(getSupportFragmentManager(), "time picker");
+            }
+        });
+
+        Button datePickButton = findViewById(R.id.setDateButton);
+        datePickButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment datePicker = new DatePickerFragment();
+                datePicker.show(getSupportFragmentManager(), "date picker");
+            }
+        });
+
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
     }
 
     @Override
     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
 
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    public void onRadioButtonClicked(View view) {
+        TextView recursiveTextView = findViewById(R.id.recursiveTextView);
+        Spinner repeat = findViewById(R.id.repeatSpinner);
+
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radioButtonOneTime:
+                if (checked)
+                    recursiveTextView.setVisibility(View.INVISIBLE);
+                    repeat.setVisibility(View.INVISIBLE);
+                    break;
+            case R.id.radioButtonRecursive:
+                if (checked)
+                    recursiveTextView.setVisibility(View.VISIBLE);
+                    repeat.setVisibility(View.VISIBLE);
+                    break;
+        }
     }
 }
