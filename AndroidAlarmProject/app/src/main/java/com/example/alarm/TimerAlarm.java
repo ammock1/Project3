d561957cdc596d7d13a26fe1aed93d94f73d3437
Casey Jonas <cjonas@ilstu.edu>
2019-04-30 16:06:54 -0500

    Added Timer Alarm Classes

--- AndroidAlarmProject/app/src/main/java/com/example/alarm/TimerAlarm.java
+++ AndroidAlarmProject/app/src/main/java/com/example/alarm/TimerAlarm.java
@@ -1,13 +1,16 @@
 package com.example.alarm;
 
-import android.app.TimePickerDialog;
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.DialogFragment;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TimePicker;
+import android.widget.TextView;
 
 public class TimerAlarm extends AppCompatActivity {
 
@@ -15,9 +18,33 @@ public class TimerAlarm extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm_by_timer);
-
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
 
+    public void onButtonClicked(View view) {
+        TextView messageTextView = findViewById(R.id.messageText);
+        String message = messageTextView.getText().toString();
+        TextView timeTextView = findViewById(R.id.timerText);
+        int mins = Integer.parseInt((String) timeTextView.getText());
+
+        scheduleTimedNotification(mins, message);
+    }
+
+    private void scheduleTimedNotification(int minutes, String message) {
+
+        Notification.Builder builder = new Notification.Builder(this);
+        builder.setContentTitle("Timer set at location "/*ADD LOCATION*/);
+        builder.setContentText(message);
+        Notification notification = builder.build();
+
+        Intent notificationIntent = new Intent(this, TimerAlarmReceiver.class);
+        notificationIntent.putExtra(TimerAlarmReceiver.NOTIFICATION_ID, 1);
+        notificationIntent.putExtra(TimerAlarmReceiver.NOTIFICATION, notification);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        long futureInMillis = SystemClock.elapsedRealtime() + (minutes * 60000);
+        AlarmManager alarmManager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+        alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, futureInMillis, pendingIntent);
     }
 }
---
+++ AndroidAlarmProject/app/src/main/java/com/example/alarm/TimerAlarmReceiver.java
@@ -0,0 +1,21 @@
+package com.example.alarm;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class TimerAlarmReceiver extends BroadcastReceiver {
+
+	public static String NOTIFICATION_ID = "id";
+	public static String NOTIFICATION = "notification";
+
+	@Override
+	public void onReceive (Context context, Intent intent) {
+		NotificationManager notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+		Notification notification = intent.getParcelableExtra(NOTIFICATION);
+		notificationManager.notify(intent.getIntExtra(NOTIFICATION_ID, 0), notification);
+	}
+}
