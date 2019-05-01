fd5ab59e5e0fb56b85e70ade7ecb5a49134d8931
Mock, Anne <ammock1.ilstu.edu>
2019-04-30 22:21:04 -0500

    Location alarm functioning

--- AndroidAlarmProject/app/src/main/java/com/example/alarm/LocAlarmObj.java
+++ AndroidAlarmProject/app/src/main/java/com/example/alarm/LocAlarmObj.java
@@ -1,7 +1,6 @@
 package com.example.alarm;
 
 import android.app.AlarmManager;
-import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -9,7 +8,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
-import android.os.PowerManager;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
@@ -18,7 +16,7 @@ public class LocAlarmObj extends BroadcastReceiver {
 
     private static LocAlarmObj locAl = new LocAlarmObj();
     Context mContext;
-    private long interval = 5000;
+    private long interval = 120000;
     NotificationChannel channel;
 
     public static LocAlarmObj getInstance(){
@@ -26,14 +24,20 @@ public class LocAlarmObj extends BroadcastReceiver {
     }
 
     public LocAlarmObj(){
+        interval = 120000;
     }
 
     public void setAlarm(Context context){
         mContext = context;
-        AlarmManager am = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
         Intent i = new Intent(context, LocAlarmObj.class);
-        PendingIntent pi = PendingIntent.getBroadcast(context, 0, i, 0);
-        am.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), interval, pi);
+        PendingIntent pi = PendingIntent.getBroadcast(context, 5, i, 0);
+        AlarmManager am = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+        if(am!=null){
+            am.cancel(pi);
+        }
+
+        am.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()+locAl.interval, pi);
+
     }
 
     public void setInterval(long inter){
@@ -43,22 +47,20 @@ public class LocAlarmObj extends BroadcastReceiver {
     public void cancelAlarm(Context context){
         mContext = context;
         Intent intent = new Intent(context, LocAlarmObj.class);
-        PendingIntent sender = PendingIntent.getBroadcast(context, 0, intent, 0);
+        PendingIntent sender = PendingIntent.getBroadcast(context, 5, intent, 0);
         AlarmManager am = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
-        am.cancel(sender);
+        if(am!=null){
+            am.cancel(sender);
+        }
     }
     @Override
     public void onReceive(Context context, Intent intent){
-       /* PowerManager pm = (PowerManager)context.getSystemService(context.POWER_SERVICE);
-        PowerManager.WakeLock w1 = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "wakey:");
-        w1.acquire();*/
+       // if(intent.getAction())
         mContext = context;
-
+        Log.i("alarm", "Location alarm received");
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             channel = new NotificationChannel("Channel1", "noteChannel", NotificationManager.IMPORTANCE_DEFAULT);
             channel.setDescription("it's a channel");
-            // Register the channel with the system; you can't change the importance
-            // or other notification behaviors after this
             NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
             notificationManager.createNotificationChannel(channel);
         }
@@ -66,10 +68,9 @@ public class LocAlarmObj extends BroadcastReceiver {
         NotificationCompat.Builder locNoteBuilder = new NotificationCompat.Builder(context, "Channel1")
                 .setContentText("Get up and walk!")
                 .setSmallIcon(R.drawable.ic_launcher_background)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                ;
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
         notificationManager.notify(1, locNoteBuilder.build());
 
-       // w1.release();
     }
+
 }
